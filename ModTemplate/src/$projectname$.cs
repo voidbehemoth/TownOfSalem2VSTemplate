804589056a0768f0510bffa839017c6cf3828bf5
VoidBehemoth <VoidBehemoth@gmail.com>
2023-10-16 15:28:46 -0600

    Add project files.

---
+++ ModTemplate/src/$projectname$.cs
@@ -0,0 +1,23 @@
+﻿using SML;
+
+namespace $safeprojectname$
+{
+    [Mod.SalemMod]
+    public class Main
+    {
+        public static void Start()
+        {
+            Utils.Logger.Log("ain't no way");
+        }
+    }
+
+
+    public static class ModInfo
+    {
+        public const string PLUGIN_GUID = "$safeprojectname$";
+
+        public const string PLUGIN_NAME = "$projectname$";
+
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
---
+++ ModTemplate/src/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace $safeprojectname$.Utils
+{
+    public class Logger
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine("[" + $projectname$.ModInfo.PLUGIN_GUID + "] " + message);
+        }
+    }
+}
