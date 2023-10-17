6f7777a8329ba6e9f74f4392abc767025a44032a
VoidBehemoth <VoidBehemoth@gmail.com>
2023-10-16 18:58:01 -0600

    Added comments

--- ModTemplate/src/Utils.cs
+++ ModTemplate/src/Utils.cs
@@ -2,11 +2,12 @@
 
 namespace $safeprojectname$.Utils
 {
+    // Not necessary for mod, just useful
     public class Logger
     {
         public static void Log(string message)
         {
-            Console.WriteLine("[" + $projectname$.ModInfo.PLUGIN_GUID + "] " + message);
+            Console.WriteLine("[" + $projectname$.ModInfo.PLUGIN_GUID + "] " + message); // Formatting is a style choice on the developer's part
         }
     }
 }
